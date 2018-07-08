8ef5c2fd9c24346def6952e6a29fadac28428466
Nazar Flome <nflome@gmail.com>
2018-07-08 15:16:04 -0700

    8

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-
-
-<script>
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
+onscroll = function() {
+    if (document.body.scrollTop > 200) {
+        navbar.style.top = "0"
+    }
+    else {
+        navbar.style.top = "2em"
     }
 }
-
-</script>
